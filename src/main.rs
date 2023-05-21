885c5f6f94efb1bd6a59711464ee5b857bb57791
Charleslf2 <datadevfernolf@gmail.com>
2023-05-21 16:51:44 -0700

    some basic implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use eframe::egui;
+use wifi_qr_code::{AuthenticationType, Visibility, WifiCredentials};
+
+
 fn main() {
-    println!("Hello, world!");
+    let native_options=eframe::NativeOptions{
+        initial_window_size:Some(egui::vec2(300.0, 200.0)),
+        resizable:false,
+        fullscreen:false,
+        ..Default::default(),
+    };
+
+    eframe::run_native(
+        "Wifi qr codes",
+        native_options, 
+
+    )
+
+}
+
+
+struct Wifi{
+    name:String,
+    ahthentification_type:String,
+    passeword:String,  
+}
+
+
+impl Default for Wifi{
+    fn default()->Self{
+
+    }
 }
