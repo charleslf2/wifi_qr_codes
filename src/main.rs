98f69876f4294f52cfed9cb3d7f331a0c3851940
Charleslf2 <datadevfernolf@gmail.com>
2023-05-21 17:13:13 -0700

    create the window

--- src/main.rs
+++ src/main.rs
@@ -4,30 +4,43 @@ use wifi_qr_code::{AuthenticationType, Visibility, WifiCredentials};
 
 fn main() {
     let native_options=eframe::NativeOptions{
-        initial_window_size:Some(egui::vec2(300.0, 200.0)),
+        initial_window_size:Some(egui::vec2(300.0, 400.0)),
         resizable:false,
-        fullscreen:false,
-        ..Default::default(),
+        maximized:false,
+        ..Default::default()
     };
 
     eframe::run_native(
         "Wifi qr codes",
         native_options, 
-
-    )
+        Box::new(|_cc|Box::<Wifi>::default()),
+    );
 
 }
 
 
 struct Wifi{
     name:String,
-    ahthentification_type:String,
-    passeword:String,  
+    auth_type:String,
+    password:String,  
 }
 
 
 impl Default for Wifi{
     fn default()->Self{
+        Self{
+            name:"CANALBOX-B9F7-2G".to_owned(),
+            auth_type:"WPA".to_owned(),
+            password:"1234".to_owned(),
 
+        }
+    }
+}
+
+impl eframe::App for Wifi{
+    fn update(&mut self, ctx:&egui::Context, _frame:&mut eframe::Frame){
+        egui::CentralPanel::default().show(ctx, |ui|{
+            ui.heading("Hello world");
+        });
     }
 }
