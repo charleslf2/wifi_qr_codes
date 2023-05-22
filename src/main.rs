ff6881b70ea003ab3b2d5c842ac9ebdd630fa17b
Charleslf2 <datadevfernolf@gmail.com>
2023-05-21 18:35:19 -0700

    now use can add save directory path

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use eframe::egui;
 use wifi_qr_code::{AuthenticationType, Visibility, WifiCredentials};
 use wifi_qr_code::QrCodeEcc;
 use std::fs::File;
+use std::path::PathBuf;
 
 
 fn main() {
@@ -25,6 +26,7 @@ struct Wifi{
     name:String,
     password:String,  
     image_name:String,
+    dir_path:String,
 }
 
 impl Wifi{
@@ -34,8 +36,9 @@ impl Wifi{
             authentication_type:AuthenticationType::WPA(String::from(self.password.to_owned())),
             visibility:Visibility::Visible,
         };
-        let png_file=File::create(self.image_name.to_owned()+ ".png").expect("failed");
+        let png_file=File::create(self.dir_path.to_owned() + &self.image_name.to_owned()+ ".png").expect("failed");
         let image= wifi_qr_code::encode_as_png(&wifi_cred, QrCodeEcc::Medium, 200, png_file);
+        //let dir_file=PathBuf::new().push(self.dir_path +png_file);
 
         println!("hello world");
     }
@@ -47,6 +50,7 @@ impl Default for Wifi{
             name:"CANALBOX-B9F7-2G".to_owned(),
             password:"1234".to_owned(),
             image_name:"new_qr_code".to_owned(),
+            dir_path:"C:/Users/Charles_lf/Desktop".to_owned(),
 
         }
     }
@@ -59,6 +63,8 @@ impl eframe::App for Wifi{
             ui.text_edit_singleline(&mut self.name);
             ui.label("The Password");
             ui.text_edit_singleline(&mut self.password);
+            ui.label("saved path");
+            ui.text_edit_singleline(&mut self.dir_path);
             ui.label("qr code name");
             ui.text_edit_singleline(&mut self.image_name);
 
